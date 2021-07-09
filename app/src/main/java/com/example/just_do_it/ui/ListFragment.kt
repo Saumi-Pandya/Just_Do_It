5f13c779889afa939afbe32fa7e59c30a61221ae
Saumi-Pandya <saumipandya@gmail.com>
2021-07-09 17:02:55 +0530

    added navigation graph

--- app/src/main/java/com/example/just_do_it/ui/ListFragment.kt
+++ app/src/main/java/com/example/just_do_it/ui/ListFragment.kt
@@ -5,18 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.just_do_it.R
+import com.example.just_do_it.data.Task
+import com.example.just_do_it.data.TaskViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class ListFragment : Fragment() {
+class ListFragment : Fragment(),TaskAdapter.onItemClickListener {
     // TODO: Rename and change types of parameters
+    lateinit var viewModel: TaskViewModel
 
-    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.listAllTask().observe(viewLifecycleOwner, Observer {
+            setRecyclerView(it)
+        })
+
+        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_saveFragment)
+        }
+    }
+
+    private fun setRecyclerView(taskList: List<Task>){
+          var recyclerView = requireView().findViewById<RecyclerView>(R.id.recycler_view)
+          recyclerView.adapter = TaskAdapter(taskList,this@ListFragment)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+    }
+
+    override fun onItemClick(position: Int) {
+
     }
 
 
--- app/src/main/java/com/example/just_do_it/ui/SaveFragment.kt
+++ app/src/main/java/com/example/just_do_it/ui/SaveFragment.kt
@@ -5,17 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
 import com.example.just_do_it.R
+import com.example.just_do_it.data.Task
+import com.example.just_do_it.data.TaskViewModel
 
 
 class SaveFragment : Fragment() {
+
+    lateinit var viewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_save, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val edittexttitle = view.findViewById<EditText>(R.id.edittexttitle)
+        val edittextdesc = view.findViewById<EditText>(R.id.edittextdesc)
+        val savebutton = view.findViewById<Button>(R.id.buttonsave)
+        val taskObject = Task(0,title =  edittexttitle.toString(),description = edittextdesc.toString())
+        savebutton.setOnClickListener {
+            saveData(taskObject)
+        }
+
+    }
+
+    fun saveData(taskObject: Task){
+        viewModel.insertTask(taskObject)
+        requireActivity().onBackPressed()
     }
 
 }
